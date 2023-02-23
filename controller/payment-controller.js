0145f140b0158f70149e543b3bfc7e6af0a75052
Anik <aniksaha859@gmail.com>
2023-02-23 12:56:45 +0530

    first commit

---
+++ controller/payment-controller.js
@@ -0,0 +1,67 @@
+
+import PaytmChecksum from "../paytm/PaytmChecksum.js";
+import { paytmMerchantkey , paytmParams} from "../server.js";
+import formidable from "formidable";
+import https from 'https';
+
+
+export const addPaymentGateway = async (request, response) => {
+        try {
+          let paytmChecksum = await PaytmChecksum.generateSignature(paytmParams, paytmMerchantkey);
+
+          let params = {
+             ...paytmParams, 'CHECKSUMHASH': paytmChecksum
+          }
+
+          response.status(200).json(params);
+        }catch(error) {
+          response.status(500).json({ error: error.message});
+        }
+}
+
+export const paytmResponse = (request, response) => {
+    const form = new formidable.IncomingForm();
+    let paytmChecksum = request.body.CHECKSUMHASH;
+    delete request.body.CHECKSUMHASH;
+
+    let isVeryfySignature = paytmchecksum.verifySignature(request.body, paytmMerchantkey, paytmChecksum);
+    if(isVeryfySignature) {
+        let paytmParams = {};
+        paytmParams['MID'] = request.body.MID;
+        paytmParams['ORDERID'] = request.body.ORDERID;
+
+        paytmchecksum.generateSignature(paytmParams, paytmMerchantkey).then(function(checksum) {
+            paytmParams['CHECKSUMHASH'] = checksum;
+
+            let post_data = JSON.stringify(paytmParams);
+
+            let options = {
+                hostname: 'securegw-stage.paytm.in',
+                port: 443,
+                path: '/order/status',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': post_data.length
+                }
+            }
+
+            let res = "";
+            let post_req = https.request(options, function(post_res) {
+                post_res.on('data', function(chunk){
+                    res += chunk;
+                });
+
+                post_res.on('end', function() {
+                    let result = JSON.parse(res)
+                    response.redirect('http://localhost:3000/')
+                })
+            });
+
+            post_req.write(post_data);
+            post_req.end();
+        })
+    }else {
+        console.log('Checksum mismatch');
+    }
+    
+}
